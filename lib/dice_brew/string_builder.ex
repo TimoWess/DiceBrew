ae080f53391060f414ac6033f03a4a97402d6b8c
Timo Wesselmann <timo.wesselmann@gmail.com>
2023-12-23 18:12:06 +0100

    update: more StringBuilder functionality

--- lib/dice_brew/string_builder.ex
+++ lib/dice_brew/string_builder.ex
@@ -1,5 +1,36 @@
 defmodule DiceBrew.StringBuilder do
+  alias DiceBrew.RollPart
+  alias DiceBrew.FixedPart
+  alias DiceBrew.PartialResult
   alias DiceBrew.Result
+
+  defp get_sign_string(sign) do
+    case sign do
+      :plus -> "+"
+      :minus -> "-"
+      true -> sign
+    end
+  end
+
+  def to_string(list) when is_list(list) do
+    "[#{list |> Enum.map(&Kernel.to_string/1) |> Enum.join(", ")}]"
+  end
+
+  @spec to_string(RollPart.t()) :: String.t()
+  def to_string(%RollPart{total: total}) do
+    "#{total}"
+  end
+
+  @spec to_string(FixedPart.t()) :: String.t()
+  def to_string(%FixedPart{value: value}) do
+    "#{value}"
+  end
+
+  @spec to_string(PartialResult.t()) :: String.t()
+  def to_string(%PartialResult{label: label, total: total, parts: parts}) do
+    parts_string = parts |> Enum.map(&__MODULE__.to_string/1) |> Enum.join(", ")
+    "#{if label != "", do: "#{label}", else: "Roll"}: [#{parts_string}] Result: #{total}"
+  end
 
   @spec to_string(Result.t()) :: String.t()
   def to_string(%Result{label: label, total: total, partial_results: _partial_results}) do
